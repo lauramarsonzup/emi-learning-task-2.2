3a80bd42dfac85273165654eaa22c48351883783
Rafael Rollo <rafaelrollo92@gmail.com>
2022-01-03 20:44:06 +0000

    Initial commit

---
+++ LearningTask-2-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningTask-2-2
+//
+//  Created by rafael.rollo on 03/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# emi-learning-task-2.2
+Explorando o Mundo iOS - Learning Task 2.2
